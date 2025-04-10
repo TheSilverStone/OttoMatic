7279351e74d74ddc7a95d95a2d5a199bf7167cd9
TheSilverStone <178652409+TheSilverStone@users.noreply.github.com>
2025-04-10 07:55:04 +0300

    set it correctly?

--- src/Player/Player_Weapons.c
+++ src/Player/Player_Weapons.c
@@ -250,12 +250,12 @@ OGLMatrix4x4	m;
 
 		case	WEAPON_TYPE_GROWTH:
 				MorphToSkeletonAnim(theNode->Skeleton, PLAYER_ANIM_DRINK, 4);
-				set GROWING_NOW
+				type GROWING_NOW = true;
 				break;
 
 		case	WEAPON_TYPE_SHRINK:
-				MorphToSkeletonAnim(theNode->Skeleton, PLAYER_ANIM_DRINK, 1);
-				set SHRINKING_NOW
+				MorphToSkeletonAnim(theNode->Skeleton, PLAYER_ANIM_DRINK, 4);
+				type SHRINKING_NOW = true;
 				break;
 
 		case	WEAPON_TYPE_FLARE:
